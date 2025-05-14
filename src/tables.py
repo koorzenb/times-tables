a1147df6c42515d5070c4ebc0190ba832be604c0
Barend Koorzen <koorzenb@gmail.com>
2025-05-14 06:11:22 -0300

    Showing high scores on startup

--- src/tables.py
+++ src/tables.py
@@ -17,11 +17,36 @@ from logic import *
 #     threading.Event().wait(2)
 #     print("Time's up!")
 
+def get_high_scores():
+    file = os.path.join(os.path.dirname(__file__), "high_scores.json")
+    if not os.path.exists(file):
+        with open(file, "w") as data_file:
+            json.dump({}, data_file)
+    else:
+        with open(file, "r") as data_file:
+            str_data = data_file.read()
+            if str_data:
+                high_scores = json.loads(str_data)
+                return high_scores
+            else:
+                return {}
 
+def show_high_scores(high_scores = None):
+    if high_scores is None:
+        high_scores = get_high_scores()
+  
+    print("\nHigh Scores:")
+    for user, record in high_scores.items():
+        print(f"- {user.title()}: {record['time']} seconds")
+
+    print("\n")
+    
 def main():
     print('\n' * 20)
     print(f"{welcome}\n")
 
+    show_high_scores()
+    
     name = input("Enter your name: ")
     name = name.lower()
     correct_answers = 0
@@ -63,32 +88,11 @@ def main():
         print(f"Sorry, {name}, you failed this time. Better luck next time!")
     print(f"You got {correct_answers} out of {NUMBER_OF_QUESTIONS} questions correct.")
 
-    updated_high_scores = {}
-    file = os.path.join(os.path.dirname(__file__), "high_scores.json")
+    updated_high_scores = add_high_scores(get_high_scores(), name, correct_answers, elapsed_time)
 
-    #check if file exists
-    if not os.path.exists(file):
-        with open(file, "w") as data_file:
-            json.dump({}, data_file)
-
-    else:
-        with open(file, "r") as data_file:
-            str_data = data_file.read()
-            if str_data:
-                high_scores = json.loads(str_data)
-                user_high_record = high_scores.get(name, [])
-                updated_high_scores = add_high_scores(high_scores, name, correct_answers, elapsed_time)
-            else:
-                updated_high_scores = add_high_scores(high_scores, name, correct_answers, elapsed_time)
-                print(f"New high score for {name}!")
-
-    with open(file, "w") as data_file:
+    with open(os.path.join(os.path.dirname(__file__), "high_scores.json"), "w") as data_file:
         json.dump(updated_high_scores, data_file)
-        print("\nHigh Scores:")
-        for user, record in updated_high_scores.items():
-            print(f"{user.title()}: {record['time']} seconds")
-
-        print("\n")
+        show_high_scores(updated_high_scores)
 
 
 
